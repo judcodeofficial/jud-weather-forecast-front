f1c544e5a1eef8c785a6ebead4e5b1e68aa2dc15
judcodeofficial <judcodeofficial@gmail.com>
2023-05-27 00:18:13 +1000

    Frontend finished

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,7 @@ module.exports = {
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
     extend: {
@@ -12,7 +13,23 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      fontFamily: {
+        'oswald': ['var(--font-oswald)'],
+        'comme': ['var(--font-comme)']
+      },
+      colors: {
+        'jud-primary': {
+          light: '#EFEFEF',
+          DEFAULT: '#001E3C',
+          dark: '#001E3C'
+        },
+        'jud-secondary': {
+          light: '#000000',
+          DEFAULT: '#132F4C',
+          dark: '#132F4C'
+        },
+      },
     },
   },
-  plugins: [],
+  plugins: [], 
 }
